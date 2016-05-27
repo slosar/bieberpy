985221d4a28a77c0ec4b3539cf95686ab6fce118
anze <anze@levstik>
2016-05-27 19:23:24 -0400

    first commit

---
+++ plot_trace.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from optparse import OptionParser
+import sys
+sys.path.append('py')
+from bieberpy import SHTrace
+
+parser = OptionParser(usage="Specify .bbr file on command line")
+parser.add_option("-s", "--show", dest="show", default=False,
+                  action="store_true", help="call show at the end")
+parser.add_option("-p", "--pdf", dest="pdf", default=None,
+                  help="save to pdf", metavar="filename")
+parser.add_option("--fmin", dest="fmin", default=None,
+                  help="min freqency [MHz]", metavar="value", type="float")
+parser.add_option("--fmax", dest="fmax", default=None,
+                  help="max freqency [MHz]", metavar="value", type="float")
+parser.add_option("--tmin", dest="tmin", default=None,
+                  help="min time [ms timestamp]", metavar="value", type="float")
+parser.add_option("--tmax", dest="tmax", default=None,
+                  help="max time [ms timestamp]", metavar="value", type="float")
+parser.add_option("--favg", dest="favg", default=1,
+                  help="frequency averaging", metavar="value", type="int")
+parser.add_option("--tavg", dest="tavg", default=1,
+                  help="time averaging", metavar="value", type="int")
+parser.add_option("--pmin", dest="pmin", default=None,
+                  help="min power [dB]", metavar="value", type="float")
+parser.add_option("--pmax", dest="pmax", default=None,
+                  help="max power [dB]", metavar="value", type="float")
+
+(o, args) = parser.parse_args()
+
+if len(args)>0:
+    fname=args[0]
+else:
+    parser.print_help()
+    sys.exit(0)
+    
+print "Reading ",fname
+da=SHTrace(fname)
+print "Frequencies span:",da.freq[0], da.freq[-1], "# samples:",len(da.freq)
+print "Timestamps span:",da.timestamps[0], da.timestamps[-1], "# samples:", len(da.timestamps)
+
+faxis,taxis,pdata=da.getDataCube(o.fmin,o.fmax,o.tmin,o.tmax,o.favg,o.tavg)
+fmin,fmax=faxis[0],faxis[-1]
+tmin,tmax=taxis[0], taxis[-1]
+print tmax-tmin
+print o.pmin, o.pmax
+plt.imshow(pdata,interpolation='nearest',extent=(fmin,fmax,0,(tmax-tmin)/1e6),
+           vmin=o.pmin, vmax=o.pmax,aspect='auto')
+plt.ylabel('t[s]')
+plt.xlabel('f[MHz]')
+plt.colorbar()
+
+if o.pdf:
+    plt.savefig(o.pdf)
+
+if o.show:
+    plt.show()
+
---
+++ py/bieberpy.py
@@ -0,0 +1,102 @@
+#
+# Python Interface to Signal Hound .bbr files
+#
+import numpy as np
+
+
+class SHTrace(object):
+
+    """
+    From https://github.com/SignalHound/BBApp/blob/master/BBApp/src/model/playback_toolbar.h:
+
+       struct playback_header {
+        unsigned short signature;
+        unsigned short version;
+
+        int sweep_count;
+
+        ushort title[MAX_TITLE_LEN + 1];
+        double center_freq; // Sweep settings
+        double span;
+        double rbw;
+        double vbw;
+        double ref_level;
+        double div;
+        int atten;
+        int gain;
+        int detector;
+
+        int trace_len;
+        double trace_start_freq;
+        double bin_size;
+    };
+    """
+    _head_desc=[('signature','u2'),('version','u2'),('count','i4'),
+                ('title','S256'),('center_freq','f8'),('span','f8'),
+                ('rbw','f8'),('vbw','f8'), ('ref_level','f8'),('div','f8'),
+                ('atten','i4'), ('gain','i4'),('detector','i4'),('trace_len','i4'),
+                ('trace_start_freq','f8'),('bin_size','f8')]
+    _head_dt=np.dtype(_head_desc,align=False)
+
+    def __init__ (self,filename):
+        """
+        Given filename, read the file and initialize the object
+        """
+        f=open(filename)
+        head=np.fromfile(f,self._head_dt,count=1)
+        for n in head.dtype.names:
+            self.__dict__.update({n:head[n][0]})
+        self.Ns=self.count
+        # now read the rest of the data
+        rec_desc=[('time','u8'),('min_trace','f4',self.trace_len),('max_trace','f4',self.trace_len)]
+        rec_dt=np.dtype(rec_desc,align=False)
+        self.data=np.fromfile(f,rec_dt)
+        if not (len(self.data)==self.count):
+            print "Warning, number of records in header (%i) doesn't match number read (%i) " %(
+                self.count, len(self.data))
+        # we work in MHz
+        self.freq = self.trace_start_freq/1e6+self.bin_size*np.arange(self.trace_len)/1e6
+        self.timestamps=self.data['time']
+    
+    def getDataCube (self,min_freq=None, max_freq=None, min_time=None, max_time=None,
+                     freq_avg=1, time_avg=1):
+        """
+        Returns the data appropriate cuts and averages
+        """
+        if min_freq==None:
+            imin=0
+        else:
+            imin=np.argmax(self.freq>min_freq)
+        if max_freq==None:
+            imax=len(self.freq)
+        else:
+            imax=np.argmin(self.freq<max_freq)
+
+        if min_time==None:
+            jmin=0
+        else:
+            jmin=np.argmax(self.timestamps>min_time)
+        if max_time==None:
+            jmax=len(self.timestamps)
+        else:
+            jmax=np.argmin(self.timestamps<max_time)
+
+        freqs=self.freq[imin:imax]
+        tstamps=self.timestamps[jmin:jmax]
+        #print imin,imax,jmin,jmax
+        data=self.data["min_trace"][jmin:jmax,imin:imax]
+        if (freq_avg>1) or (time_avg>1):
+            ## funnily enough, there doesn't seem to be a better way
+            ## of boxcar average than this
+            Nf=len(freqs)/freq_avg
+            Nt=len(tstamps)/time_avg
+            nfreqs=np.array([freqs[i*freq_avg:(i+1)*freq_avg].mean() for i in range(Nf)])
+            ntstamps=np.array([tstamps[i*time_avg:(i+1)*time_avg].mean() for i in range(Nt)])
+            ndata=np.array([[ data[i*time_avg:(i+1)*time_avg,j*freq_avg:(j+1)*freq_avg].mean() for j in range(Nf)]
+                                              for i in range(Nt)])
+            freqs=nfreqs
+            tstamps=ntstamps
+            data=ndata
+        return freqs,tstamps,data
+    
+        
